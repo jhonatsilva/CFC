1e26fa99b33672f7b0ffce15f78712cfa84c744e
jhonatsilva <dasilva.jhonathan@icloud.com>
2025-04-09 21:18:39 -0300

    Alter the code to see the code working on log window

--- CarFactory/src/A02_UP/Main.java
+++ CarFactory/src/A02_UP/Main.java
@@ -2,42 +2,56 @@ package A02_UP;
 
 import java.util.Random;
 
+//Classe principal que inicia a simulação
 public class Main {
-    public static void main(String[] args) {
-        Estoque estoque = new Estoque();
+ public static void main(String[] args) {
+     System.out.println("[SISTEMA] Iniciando simulação de produção de veículos...");
 
-        // Cria esteiras de saída da fábrica (4 estações)
-        EsteiraCircular<Carro>[] esteiras = new EsteiraCircular[4];
-        for (int i = 0; i < 4; i++) esteiras[i] = new EsteiraCircular<>(40);
+     Estoque estoque = new Estoque();
 
-        // Inicia os funcionários (5 por estação)
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; j++) {
-                new Funcionario(j, i, estoque, esteiras[i]).start();
-            }
-        }
+     // Cria esteiras de saída da fábrica (4 estações)
+     EsteiraCircular<Carro>[] esteiras = new EsteiraCircular[4];
+     for (int i = 0; i < 4; i++) {
+         esteiras[i] = new EsteiraCircular<>(40);
+         System.out.println("[SISTEMA] Esteira da estação " + i + " criada.");
+     }
 
-        // Cria 3 lojas
-        Loja[] lojas = new Loja[3];
-        for (int i = 0; i < 3; i++) lojas[i] = new Loja("loja" + i);
+     // Inicia os funcionários (5 por estação)
+     for (int i = 0; i < 4; i++) {
+         for (int j = 0; j < 5; j++) {
+             new Funcionario(j, i, estoque, esteiras[i]).start();
+             System.out.println("[SISTEMA] Funcionario " + j + " da estação " + i + " iniciado.");
+         }
+     }
 
-        // Thread que envia os carros da fábrica para as lojas
-        new Thread(() -> {
-            while (true) {
-                try {
-                    for (int i = 0; i < 4; i++) {
-                        Carro carro = esteiras[i].remover();
-                        lojas[new Random().nextInt(3)].receberCarro(carro);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+     // Cria 3 lojas
+     Loja[] lojas = new Loja[3];
+     for (int i = 0; i < 3; i++) {
+         lojas[i] = new Loja("loja" + i);
+         System.out.println("[SISTEMA] Loja " + lojas[i].nome + " criada.");
+     }
 
-        // Inicia os 20 clientes
-        for (int i = 0; i < 20; i++) {
-            new Cliente("cliente" + i, lojas).start();
-        }
-    }
+     // Thread que envia os carros da fábrica para as lojas
+     new Thread(() -> {
+         while (true) {
+             try {
+                 for (int i = 0; i < 4; i++) {
+                     Carro carro = esteiras[i].remover();
+                     Loja lojaDestino = lojas[new Random().nextInt(3)];
+                     lojaDestino.receberCarro(carro);
+                 }
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+     }).start();
+
+     // Inicia os 20 clientes
+     for (int i = 0; i < 20; i++) {
+         new Cliente("cliente" + i, lojas).start();
+         System.out.println("[SISTEMA] Cliente cliente" + i + " iniciado.");
+     }
+
+     System.out.println("[SISTEMA] Simulação em execução. Acompanhe os logs no console e nos arquivos.");
+ }
 }
